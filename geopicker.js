1f03184b6acca70e514da52143109b2f1b72b613
Diego Bernardi <bernardiego@gmail.com>
2021-10-29 11:15:04 -0300

    First commit.
    Changes:
    
    - Improved library loading. Don't repeat the load (for every mbtile-capable question)
      and add capability to load more.
    - Use jquery to create/access node elements
    - Accurately detect when the map and points are loaded/added.
    - Add a "current position" marker in the map on load. This marker is not considered
      as a point for the answer.
    - Adaptive zoom: always zoom to a relevant point when a change in map is made.
      The relevance is given by this order:
      1) Added point 2) Added mbtile 3) Current position marker.
    - Added the capability to change the point added' icon.
    - Show point coordinates instead of search box when screen is < 720px.
    - Add a config.js file to store private/useful variables.
    - Fix some mbtile addition/removal bugs.
    - Added gitignore.
    - Modified styles.
    - Added scriptjs as dependency.

--- geopicker.js
+++ geopicker.js
@@ -1,166 +1,344 @@
 /*
+ * Diego Bernardi <bernardiego@gmail.com>
  * Antea Group.
- * 
  */
 
 import Geopicker from '../../node_modules/enketo-core/src/widget/geo/geopicker';
+import { getCurrentPosition } from '../../node_modules/enketo-core/src/js/geolocation';
 import $ from 'jquery';
-import "./leaflet.mbtiles";
 import L from 'leaflet';
 import scriptjs from 'scriptjs';
 
-/** 
- * We override the initial Geopicker Widget 
- * And adding the mbtiles support on Leaflet.
- * Mbtiles files are stored in the indexedDB for the offline mode.
- * and the mbtile is automaticly load when you refresh the page.
- *
- * Know limitations :
- * - geocoder reset the map and delete mbtiles
- * - Not test with used in conjunction with other appearances
- * - Not test on Grid theme
-*/
+const isUndefined = (attribute) => typeof attribute === "undefined";
+const isNumber = (attribute) => typeof attribute === "number";
+const isFunction = (attribute) => typeof attribute === "function";
+const isString = (attribute) => typeof attribute === "string";
+const isPureObject = (attribute) => typeof attribute === "object" && attribute !== null && !Array.isArray(attribute);
+
+// Make your custom config file.
+import { custom_options } from './config';
+const options = custom_options || {};
+const maxZoom = options && options.map && isNumber(options.map.maxZoom) ? options.map.maxZoom : 24;
+const markerColor = options && options.map && isString(options.map.markerColor) ? options.map.markerColor : "blue";
+const markerSize = options && options.map && options.map.markerSize === "big" ? options.map.markerSize : "small";
+const mbtilesPath = options && isString(options.mbtilesPath) && options.mbtilesPath !== "" ? options.mbtilesPath : undefined;
+
 export default class GeopickerMbTiles extends Geopicker {
 
-    //////////////////////////// Private methods //////////////////////////////////
-    ///////////////////////////////////////////////////////////////////////////////
-    __getQuestionId() {
-        var formId = $('form')[0].id;
-        return $(this.question).find("input[name*='" + formId + "']").prop("name")
-    }
+   __getQuestionId() {
+      var formId = $('form').attr("id") || $('form').attr("data-form-id");
+      return $(this.question).find("input[name*='" + formId + "']").prop("name");
+   }
 
-    __removeMBLayer() {
-        var self = this;
-        // Remove from map
-        self.map.removeLayer(self.mbLayer);
-        // Init the transaction
-        var tx = self.db.transaction("mbtiles", "readwrite");
-        var store = tx.objectStore("mbtiles");
-        // Execute the delete transaction
-        store.delete(self.idQuestion); // id based on the question
-    }
+   __getIcon() {
+      let self = this;
+      const posible_colors = ["black", "blue", "gold", "green", "grey", "orange", "red", "violet", "yellow"];
+      const color = isString(markerColor) && posible_colors.indexOf(markerColor) > -1 ? markerColor : "blue";
+      const size = markerSize === "big" ? "-2x" : "";
 
-    __addMbTiles(files, self) {
-        // Remove mbtiles files if already exist
-        typeof self.mbLayer !== "undefined" ? self.__removeMBLayer() : null;
-        if (!self.icountCheckMap) {
-            self.icountCheckMap = 0;
-        }
-        self.icountCheckMap++;
+      if (isPureObject(self.markerIcon) && self.markerIcon.color === color && self.markerIcon.icon instanceof L.Icon)
+         return self.markerIcon.icon;
 
-        // Only if map exist
-        if (typeof self.map !== "undefined") {
-            let tmppath = URL.createObjectURL(files[0]);
-            self.mbLayer = L.tileLayer.mbTiles(tmppath).addTo(self.map);
-            self.mbLayer.bringToFront();
-            // Loaded or Error
-            self.mbLayer.on('databaseloaded', function (ev) {
-                // Init the transaction
-                var tx = self.db.transaction("mbtiles", "readwrite");
-                var store = tx.objectStore("mbtiles");
-                // Add the blob path
-                store.put({path: files, mbId: self.idQuestion});
-                // Remove loader on complete
-                tx.oncomplete = function() {
-                    $(self.question).find('.loader').css('display', "none");
-                };
-            });
-            // Callback on database error
-            self.mbLayer.on('databaseerror', function (ev) {
-                console.info('MBTiles DB error', ev);
-                $(self.question).find('.loader').css('display', "none");
-            });
-        } else {
-            // Sometimes, self.map is not yet init. We re-launch the __addMbTiles until is init.
-			// With a max re-launch
-			// Todo : Need to change this
-            if (self.icountCheckMap < 10) {
-                setTimeout(function() {
-                    self.__addMbTiles(files, self)
-                }, 250);
-            } else {
-                return false;
-            }
-        }
-    }
+      self.markerIcon = {
+         color: color,
+         icon: new L.Icon({
+            iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon${size}-${color}.png`,
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+            iconSize: size === "" ? [18, 30] : [25, 41],
+            iconAnchor: size === "" ? [9, 30] : [12, 41],
+            popupAnchor: size === "" ? [1, -24] : [1, -34],
+            shadowSize: size === "" ? [30, 30] : [41, 41]
+         })
+      }
 
-    __initContent () {
-        var self = this;
-        // Create html elements
-        let mbtilesdiv = document.createElement("div");
-        mbtilesdiv.setAttribute("class", "mbtilesdiv");
-        let trash = document.createElement("button");
-        trash.setAttribute("class", "fa fa-trash");
-        let filechooser = document.createElement("input");
-        filechooser.setAttribute("type", "file");
-        filechooser.setAttribute("accept", ".mbtiles");
-        filechooser.setAttribute("name", "mbtiles");
+      return self.markerIcon.icon;
+   }
 
-        // Listeners input and trash
-        filechooser.addEventListener('change', function(event) {
-            $(self.question).find('.loader').css('display', "block");
-            self.__addMbTiles(event.target.files, self);
-        })
-        trash.onclick = function () {
-            typeof self.mbLayer !== "undefined" ? self.__removeMBLayer() : null;
-            filechooser.value = "";
-        };
+   __onMapReady() {
+      let self = this;
+      return new Promise(async (resolve) => {
+         while (isUndefined(self.map))
+            await new Promise(r => setTimeout(r, 500));
+         resolve();
+      });
+   }
 
-        // Append html elements
-        mbtilesdiv.appendChild(filechooser);
-        mbtilesdiv.appendChild(trash);
-        $(self.question).find("div.map-canvas-wrapper")[0].appendChild(mbtilesdiv);
-    }
-    ///////////////////////////////////////////////////////////////////////////////
+   __hasValidPoint() {
+      return this.points[0].length === 4 && isNumber(this.points[0][0]) && isNumber(this.points[0][1]);
+   }
 
-    __onLibIsLoad() {
-        var self = this;
-        // Get id question
-        self.idQuestion = self.__getQuestionId();
-        // Add loader element
-        $(self.question).find(".map-canvas").append("<div class='loader'></div>")
+   __getPoint() {
+      let self = this;
+      return new Promise(async (resolve) => {
+         let stop = 100;
+         const step = 100;
+         while (!self.__hasValidPoint() && stop > 0) {
+            stop -= step;
+            await new Promise(r => setTimeout(r, step));
+         }
+         if (!self.__hasValidPoint())
+            return resolve(undefined);
 
-        // Connexion to the indexedDB (TODO : maybe change the dbname for match with Enketo ?)
-        var request = window.indexedDB.open("enketoMbtiles", 2);
-        request.onupgradeneeded = function(e) {
-            // The database did not previously exist, so create object stores and indexes.
-            self.db = e.target.result;
-            self.db.objectStoreNames.contains( "mbtiles" ) || self.db.createObjectStore( "mbtiles", { keyPath: "mbId"} );
-        };
+         resolve(
+            L.featureGroup([
+               L.marker([self.points[0][0], self.points[0][1]], {
+                  clickable: false,
+                  draggable: false,
+                  keyboard: false,
+                  opacity: 0
+               })
+            ])
+         );
+      });
+   }
 
-        request.onsuccess = function() {
-            // Access to the DB
-            self.db = request.result;
-            var tx = self.db.transaction("mbtiles", "readonly");
+   __onPointAddition(callback = undefined) {
+      let self = this;
+      if (!isFunction(callback))
+         return;
+      new MutationObserver(
+         callback
+      ).observe(
+         self.$widget.find(".leaflet-marker-pane")[0],
+         {
+            childList: true
+         }
+      );
+   }
+
+   __changeMarkerIcon() {
+      let self = this;
+      let icon = self.__getIcon();
+      if (isPureObject(self.markerLayer))
+         self.markerLayer.eachLayer(l => {
+            l.setIcon(icon);
+         });
+   }
+
+   __adaptiveZoom() {
+      let self = this;
+      return new Promise(async (resolve) => {
+         let maxZ = maxZoom;
+         let mbtilesBound = undefined;
+         if (self.map.hasLayer(self.mbLayer)) {
+            mbtilesBound = self.mbLayer._bounds;
+            if (isPureObject(self.mbLayer.options) && isNumber(self.mbLayer.options.maxZoom))
+               maxZ = self.mbLayer.options.maxZoom;
+         }
+         self.map.setMaxZoom(maxZ);
+         let lPoint = await self.__getPoint();
+         $(".pointcoords").html("");
+         if (isPureObject(lPoint)) {
+            self.map.fitBounds(lPoint.getBounds());
+            $(".pointcoords").html(`Lat: ${self.points[0][0].toFixed(6)}°, Lon: ${self.points[0][1].toFixed(6)}° ${isNumber(self.points[0][3]) ? "(" + self.points[0][3].toFixed(3) + "m)" : ""}`);
+            return resolve();
+         } else if (!isUndefined(mbtilesBound)) {
+            let north = self.mbLayer._bounds.getNorth();
+            if (north !== -Infinity && north !== Infinity)
+               self.map.fitBounds(self.mbLayer._bounds, { paddingTopLeft: [5, 5], paddingBottomRight: [5, 5], animate: false });
+            return resolve();
+         } else if (isPureObject(self.currentPositionPoint))
+            self.map.fitBounds(self.currentPositionPoint.getBounds());
+         resolve();
+      });
+   }
+
+   __AddCurrentPositionMarker() {
+      var self = this;
+      getCurrentPosition({
+         enableHighAccuracy: true,
+         maximumAge: 0
+      }).then(result => {
+         let { latitude, longitude, altitude, accuracy } = result.position.coords;
+         let latLng = [latitude, longitude, altitude, accuracy];
+         if (self._isValidLatLng(latLng)) {
+            self.currentPositionPoint = L.featureGroup([
+               L.marker(self._cleanLatLng(latLng), {
+                  icon: L.divIcon({
+                     iconSize: 14,
+                     className: 'red-round-geopoint-marker'
+                  }),
+                  title: "my position",
+                  clickable: false,
+                  draggable: false,
+                  keyboard: false,
+                  opacity: 0.9
+               })
+            ]);
+            self.currentPositionPoint.addTo(self.map);
+            self.map.fitBounds(self.currentPositionPoint.getBounds());
+         }
+      }).catch(() => {
+         console.error('error occurred trying to obtain position');
+      });
+   }
+
+   __removeMBLayer() {
+      var self = this;
+      if (isUndefined(self.map) || isUndefined(self.mbLayer))
+         return;
+
+      if (self.map.hasLayer(self.mbLayer)) {
+         self.map.removeLayer(self.mbLayer);
+         self.mbLayer = undefined;
+      }
+      var tx = self.db.transaction("mbtiles", "readwrite");
+      var store = tx.objectStore("mbtiles");
+
+      store.delete(self.idQuestion);
+   }
+
+   __fitMbtilesBounds(force = false) {
+      let self = this;
+      if (isUndefined(self.map)) {
+         console.warn("function fitBounds: map is undefined");
+         return;
+      }
+      if (isUndefined(self.mbLayer)) {
+         console.warn("function fitBounds: layer is undefined.");
+         return;
+      }
+      if (isUndefined(self.mbLayer._bounds)) {
+         if (!self.map.hasLayer(self.mbLayer)) {
+            console.warn("function fitBounds: layer is not loaded in the map.");
+            return;
+         }
+         if (isUndefined(self.mbLayer._db)) {
+            console.warn("function fitBounds: layer' database isn't loaded yet (called on databaseloaded?).");
+            return;
+         }
+         let boundsStr = self.mbLayer._db
+            .prepare(`SELECT value FROM metadata WHERE name = 'bounds'`)
+            .getAsObject({})
+            .value;
+         if (isUndefined(boundsStr)) {
+            console.warn("function fitBounds: mbtiles files doesn't contain a 'bounds' metadata.");
+            return;
+         }
+
+         let boundsArr = boundsStr.split(",");
+         if (boundsArr.length !== 4) {
+            console.warn(`function fitBounds: 'bounds' metadata is badly formatted: ${boundsStr}`);
+            return;
+         }
+
+         self.mbLayer._bounds = L.latLngBounds(
+            L.latLng(boundsArr[1], boundsArr[0]),
+            L.latLng(boundsArr[3], boundsArr[2])
+         );
+      }
+      if (!isUndefined(self.mbLayer._bounds)) {
+         let north = self.mbLayer._bounds.getNorth();
+         if (north !== -Infinity && north !== Infinity && (!self.__hasValidPoint() || force))
+            self.map.fitBounds(self.mbLayer._bounds, { paddingTopLeft: [5, 5], paddingBottomRight: [5, 5], animate: false });
+      }
+   }
+
+   __addMbTiles(files) {
+      let self = this;
+      self.__onMapReady().then(() => {
+         self.__removeMBLayer();
+         let tmppath = URL.createObjectURL(files[0]);
+         self.mbLayer = L.tileLayer.mbTiles(tmppath).addTo(self.map);
+         self.mbLayer.bringToFront();
+         self.mbLayer.on('databaseloaded', (ev) => {
+            var tx = self.db.transaction("mbtiles", "readwrite");
             var store = tx.objectStore("mbtiles");
-            var dataRQ = store.get(self.idQuestion); // id based on the question
-            dataRQ.onsuccess = function() {
-                if (dataRQ.result !== undefined) {
-                    console.info("Mbtiles", dataRQ.result);
-                    $(self.question).find('.loader').css('display', "block");
-                    // Update input file
-                    $(self.question).find('input[name="mbtiles"]').prop('files', dataRQ.result.path)
-                    self.__addMbTiles(dataRQ.result.path, self);
-                } else {
-                    //console.info("No such mbtiles");
-                }
+            store.put({ path: files, mbId: self.idQuestion });
+            tx.oncomplete = () => {
+               self.__fitMbtilesBounds();
+               $(self.question).find('.loader').css('display', "none");
             };
-        };
+         });
+         self.mbLayer.on('databaseerror', (ev) => {
+            console.info('MBTiles DB error', ev);
+            $(self.question).find('.loader').css('display', "none");
+         });
+      });
+   }
 
-        // Initialization of the widget
-        self.__initContent();
-    }
-    
-    _init() {
-        // Initilialization
-        super._init();
-        var self = this;
-        // Prepare indexedDB only if appearance is "mbtiles" and the question has a map-canvas class
-        if (this._getProps().appearances.includes("mbtiles") && $(self.question).find(".map-canvas")) {
-            // Find better solution to import sql.js, maybe with import or require
-            scriptjs('https://unpkg.com/sql.js@0.3.2/js/sql.js', function () {
-                self.__onLibIsLoad();
-            })
-        }
-    }
+   __initContent() {
+      var self = this;
+      $(self.question).find(".input-group").append($("<div class='pointcoords'></div>"));
+
+      let mbtilesdiv = $("<div class='mbtilesdiv'></div>");
+
+      let path = $("<div class='mbtilespath'></div>");
+      if (isString(mbtilesPath)) {
+         path.html(mbtilesPath)
+         mbtilesdiv.attr("style", "padding-top: 23px;");
+         path.attr("style", "display: flex;");
+      }
+      mbtilesdiv.append(path);
+
+      let filechooser = $("<input type='file' accept='.mbtiles' name='mbtiles'/>");
+      filechooser.on('change', (event) => {
+         if (event.target.files.length === 1) {
+            $(self.question).find('.loader').css('display', "block");
+            self.__addMbTiles(event.target.files);
+         }
+      })
+      mbtilesdiv.append(filechooser);
+
+      let toBound = $("<button class='fa fa-expand'></button>");
+      toBound.on("click", () => self.__fitMbtilesBounds(true));
+      mbtilesdiv.append(toBound);
+
+      let trash = $("<button class='fa fa-trash'></button>");
+      trash.on("click", async () => {
+         self.__removeMBLayer();
+         filechooser.val("");
+         self.__adaptiveZoom();
+      });
+      mbtilesdiv.append(trash);
+
+      $(self.question).find("div.map-canvas-wrapper").append(mbtilesdiv);
+   }
+
+   __onLibIsLoad() {
+      var self = this;
+      self.idQuestion = self.__getQuestionId();
+      $(self.question).find(".map-canvas").append("<div class='loader'></div>");
+
+      var request = window.indexedDB.open("enketoMbtiles", 2);
+      request.onupgradeneeded = (e) => {
+         self.db = e.target.result;
+         self.db.objectStoreNames.contains("mbtiles") || self.db.createObjectStore("mbtiles", { keyPath: "mbId" });
+      };
+      request.onsuccess = () => {
+         self.db = request.result;
+      };
+
+      // Initialization of the widget
+      self.__initContent();
+
+      // Actions to perform once the map is loaded.        
+      self.__onMapReady().then(() => {
+         self.__AddCurrentPositionMarker();
+         self.__onPointAddition(async (records) => {
+            records.forEach(record => {
+               if (record.addedNodes.length > 0)
+                  self.__changeMarkerIcon();
+            });
+            await self.__adaptiveZoom();
+         });
+      })
+   }
+
+   _init() {
+      super._init();
+      var self = this;
+      if (isUndefined(window.geopicker))
+         window.geopicker = [];
+      window.geopicker.push(this);
+      if (self._getProps().appearances.includes("mbtiles") && $(self.question).find(".map-canvas")) {
+         if (window.geopicker.length === 1)
+            Promise.all([
+               scriptjs('https://unpkg.com/sql.js@0.3.2/js/sql.js'),
+               scriptjs('https://unpkg.com/Leaflet.TileLayer.MBTiles@1.0.0/Leaflet.TileLayer.MBTiles.js')
+            ]).then(() => self.__onLibIsLoad());
+         else
+            self.__onLibIsLoad();
+      }
+   }
 }
